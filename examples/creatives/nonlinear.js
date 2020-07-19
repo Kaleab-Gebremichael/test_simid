cda57548d6b76a437e52845c45de5a51b20549f2
Kaleab G <kaleabsolomon470@gmail.com>
2020-07-19 01:20:58 -0500

    Working first version

---
+++ examples/creatives/nonlinear.js
@@ -0,0 +1,30 @@
+/**
+ * A sample SIMID ad that shows an overlay
+ */
+class NonLinear extends BaseSimidCreative {
+  constructor() {
+    super();
+
+    this.addButtonClickActions_();
+  }
+
+  /**
+   * Adds actions to different buttons available on the overlay.
+   */
+  addButtonClickActions_() {
+    this.sendMessageOnButtonClick_("close_ad", CreativeMessage.REQUEST_STOP);
+  }
+
+  /**
+   * Listens for a click event on a button
+   * @param {String} elementName The name of the element.
+   * @param {String} message The message to send to the player.
+   * @private
+   */
+  sendMessageOnButtonClick_(elementName, message) {
+    const sendMessageFunction = () => {this.simidProtocol.sendMessage(message);}
+    document.getElementById(elementName).addEventListener(
+        'click', sendMessageFunction.bind(this));
+  }
+
+}
